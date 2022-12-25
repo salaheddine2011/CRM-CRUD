f729895554ab6c8df8387503d69c0aa26b693b5b
salaheddine benkhanous <benkhanoussalaheddine@gmail.com>
2022-12-25 19:00:17 +0100

    Changing to get Method

--- src/java/com/luv2code/springdemo/controller/CustomerController.java
+++ src/java/com/luv2code/springdemo/controller/CustomerController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -23,7 +24,7 @@ public class CustomerController {
     @Autowired
     private CustomerDAO customerDAO;// will search for an class that implements the CustomerDAO
     
-    @RequestMapping("/list")
+    @GetMapping("/list")
     public String listCustomers(Model theModel){
         
         //get customers from dao
